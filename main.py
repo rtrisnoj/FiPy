e91a266d56a5501d4771c46e6203e98b61864447
Ryan Trisnojoyo <ryan.trisnojoyo@uscubed.com>
2019-01-21 12:01:11 -0800

    NEXT test

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 from machine import ADC
 from machine import Pin
+from network import Sigfox
+import socket
 import time
+
 
 #initiliaze massa sensor configuration (ADC)
 adc = ADC()
@@ -19,12 +22,32 @@ floatValue = p0.value()
 # initiliaze relay (GPIO)
 p2 = Pin('P2',mode=Pin.OUT)
 p2.value(0)
+
+#Initiliaze Sigfox
+
+def sendMessage():
+    #Sigfox
+    sfx = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ2) # init Sigfox for RCZ2 (USA)
+    #sfx.config()
+    #import binascii
+    #print(binascii.hexlify(sfx.id()))
+    #print(binascii.hexlify(sfx.pac()))
+    s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW) # create a Sigfox Socket
+    #s.setblocking(True) #make the socket blocking
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False) # configure it as uplink only
+    s.send(str(P13.value()))
+    #s.send(str(p0.value()))
+
+
+
 while True:
     p2.value(0)
     time.sleep(5) #wait for 5 seconds
-
+    if (counter)
     print("Massa value:" + str(P13.value()))
     print("Float value:" + str(p0.value()))
 
+    #sendMessage()
+
     p2.value(1)
-    time.sleep(1755) #sleep for 30 mins
+    time.sleep(295) #sleep for 30 mins
