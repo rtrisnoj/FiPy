6c3ab0b43c78b73bfc0eba08197ff8408b653677
Ryan Trisnojoyo <ryan.trisnojoyo@uscubed.com>
2019-01-31 09:00:06 -0800

    test

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
 
+import machine
 from machine import ADC
 from machine import Pin
 from network import Sigfox
 from deepsleep import DeepSleep
+import deepsleep
 import socket
 import time
 import binascii
+import sys
 
 #initiliaze massa sensor configuration (ADC)
 adc = ADC()
 adc.vref(3130)
 
-p13 = adc.channel(pin='P13',attn=ADC.ATTN_11DB)
+pin_massa = adc.channel(pin='P13',attn=ADC.ATTN_11DB)
 
 #initiliaze float sensor (GPIO pull up)
-p0 = Pin('P0',mode=Pin.IN, pull=Pin.PULL_UP)
+pin_float_status = 1
+pin_float = Pin('P0', mode=Pin.IN, pull=Pin.PULL_UP)
     #floatValue = 1 if not tripped
     #floatValue = 0 if tripped, send alarm
 
 # initiliaze relay (GPIO)
-p2 = Pin('P2',mode=Pin.OUT)
-p2.value(0)
+pin_relay = Pin('P2',mode=Pin.OUT)
+pin_relay.value(0)
 
 #Initiliaze Sigfox
 def sendMessage():
     #Sigfox
     sfx = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ2) # init Sigfox for RCZ2 (USA)
-    #sfx.config()
-    #import binascii
-    #print(binascii.hexlify(sfx.id()))
-    #print(binascii.hexlify(sfx.pac()))
     s = socket.socket(socket.AF_SIGFOX, socket.SOCK_RAW) # create a Sigfox Socket
-    #s.setblocking(True) #make the socket blocking
-    s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False) # configure it as uplink only
-    s.send(str(P13.value()))
-    #s.send(str(p0.value()))
 
+
+    s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False) # configure it as uplink only
+    s.send(str(massaHex))
+    s.send(str(floatHex))
+    print("Sigfox Message Sent")
 
 
 while True:
-    p2.value(0)
+    pin_relay.value(0)
     time.sleep(5) #wait for 5 seconds
     #if (counter)
-    temp1 = "%04x" % p13.value()
-    temp2 = "%04x" % p0.value()
-    valueP13 = str(hex(p13.value())).lstrip("0x")
-    #print(valueP13.ljust(4, '0'))
-    print("Massa Value(Hex):" + temp1)
-    #print (valueP13)
-    print("Float Value:" + temp2)
-    #print("Massa value:" + str(P13.value()))
-    #print("Float value:" + str(p0.value()))
+    massaHex = "%04x" % pin_massa.value()
+    floatHex = "%04x" % pin_float.value()
+    #valueP13 = str(hex(pin_massa.value())).lstrip("0x")
+    print("Massa Value(Hex):" + massaHex)
+    print("Float Value:" + floatHex)
 
-    #sendMessage()
 
-    p2.value(1)
-    time.sleep(295) #sleep for 30 mins
+    sendMessage()
+    pin_relay.value(1)
+
+    #pin_float.callback(Pin.IRQ_FALLING | Pin.IRQ_RISING, pin_high_handler, pin_float)
+
+    time.sleep(60) #sleep for 30 mins
